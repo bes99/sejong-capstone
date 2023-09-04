cfb79c6a916c783e1b00db27b30cb1ba7f2eb0cd
bes99 <86958447+bes99@users.noreply.github.com>
2023-09-04 20:25:33 +0900

    [feat] postgresql 연결 및 user entity 작성

---
+++ src/main/java/com/sejong/capstone/user/User.java
@@ -0,0 +1,23 @@
+package com.sejong.capstone.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@Table(name = "user_tb")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String sex;
+}
